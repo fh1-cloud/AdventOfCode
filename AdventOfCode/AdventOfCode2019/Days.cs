cd943d6f41ed4adb0b89a12f6551c4ec0f35dd9e
fh1-cloud <fredrik.hernandez@gmail.com>
2020-12-31 00:03:51 +0100

    Started 2019

---
+++ AdventOfCode/AdventOfCode2019/Days.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2019
+{
+   public class Days
+   {
+
+
+
+   /*STATIC METHODS*/
+   #region
+
+      public static void Dec01()
+      {
+
+
+
+      }
+
+
+
+   #endregion
+
+
+   }
+}
