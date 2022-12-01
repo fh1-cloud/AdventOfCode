4d55acc1d8b74520f9b34d68aebccd83b617bdaf
Fredrik Børsum Hernandez <fbh@aaj.no>
2022-12-01 18:45:55 +0100

    dec01

---
+++ AdventOfCode/AdventOfCode2022/Classes/ElfWithCalories.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Classes
+{
+   internal class ElfWithCalories
+   {
+
+   /*LOCAL CLASSES*/
+   #region
+   #endregion
+
+   /*ENUMS*/
+   #region
+   #endregion
+
+   /*MEMBERS*/
+   #region
+   #endregion
+
+   /*CONSTRUCTORS*/
+   #region
+      public ElfWithCalories( )
+      {
+         this.Food = new List<long>( );
+      }
+   #endregion
+
+   /*PROPERTIES*/
+   #region
+      public List<long> Food { get; set; }
+      public long TotalCalories
+      {
+         get
+         {
+            return this.Food.Sum( );
+         }
+      }
+   #endregion
+
+   /*OPERATORS*/
+   #region
+   #endregion
+
+   /*METHODS*/
+   #region
+   #endregion
+
+   /*STATIC METHODS*/
+   #region
+   #endregion
+
+
+   }
+}
---
+++ AdventOfCode/AdventOfCode2022/Days.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode2022.Classes;
+
+namespace AdventOfCode2022
+{
+   public class Days
+   {
+
+      /// <summary>
+      /// Dec1
+      /// </summary>
+      public static void Dec01( )
+      {
+      //Parse input file..
+         string[ ] inp = GlobalMethods.GetInputStringArray( "..\\..\\Inputs\\Dec01.txt" );
+         
+      //Create elfs..
+         ElfWithCalories currentElf = new ElfWithCalories( );
+         List<ElfWithCalories> allElves = new List<ElfWithCalories>( );
+         allElves.Add( currentElf );
+         for( int i = 0; i < inp.GetLength( 0 ); i++ )
+         {
+         //Create new elf if reached end..
+            if( inp[i].Length == 0 )
+            {
+               currentElf = new ElfWithCalories( );
+               allElves.Add( currentElf );
+               continue;
+            }
+
+         //Add the food item..
+            currentElf.Food.Add( int.Parse( inp[i] ) );
+         }
+
+         //Find max elf..
+         //List<long> calList = allElves.Select( x => x.TotalCalories ).ToList( );
+         //Console.WriteLine( "Max calories of one elf: " + maxCal.ToString( ) );
+
+         List<ElfWithCalories> sorted = allElves.OrderBy( x => x.TotalCalories ).ToList( );
+         sorted.Reverse( );
+         long totSum = sorted[0].TotalCalories + sorted[1].TotalCalories + sorted[2].TotalCalories;
+
+         Console.WriteLine( "Max calories of top three elves: " + totSum.ToString( ) );
+
+
+      }
+
+   }
+}
