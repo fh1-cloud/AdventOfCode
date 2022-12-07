c892f7ca78e20d107879bc1bc6dfca6646d0273b
fh1-cloud <fredrik.hernandez@gmail.com>
2022-12-07 21:51:25 +0100

    dec07 started

---
+++ AdventOfCode/AdventOfCode2022/Classes/DirectoryFile.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Classes
+{
+   public class DirectoryFile
+   {
+
+   /*ENUMS*/
+   #region
+   #endregion
+
+   /*LOCAL CLASSES*/
+   #region
+   #endregion
+
+   /*MEMBERS*/
+   #region
+      protected string m_Name = null;
+      protected int m_Size = -1;
+   #endregion
+
+   /*CONSTRUCTORS*/
+   #region
+      public DirectoryFile( string name, int size )
+      {
+         m_Size = size;
+         m_Name = name;
+      }
+   #endregion
+
+   /*PROPERTIES*/
+   #region
+      public string Name { get { return m_Name; } }
+      public int Size { get { return m_Size; } }
+   #endregion
+
+   /*OPERATORS*/
+   #region
+   #endregion
+
+   /*STATIC METHODS*/
+   #region
+   #endregion
+
+   /*METHODS*/
+   #region
+   #endregion
+
+
+   }
+}
---
+++ AdventOfCode/AdventOfCode2022/Classes/DirectoryFolder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Classes
+{
+   public class DirectoryFolder
+   {
+
+   /*ENUMS*/
+   #region
+   #endregion
+
+   /*LOCAL CLASSES*/
+   #region
+   #endregion
+
+   /*MEMBERS*/
+   #region
+      protected string m_Name = null;
+      protected List<DirectoryFolder> m_Directories = new List<DirectoryFolder>( );
+      protected List<DirectoryFile> m_Files = new List<DirectoryFile>( );
+   #endregion
+
+   /*CONSTRUCTORS*/
+   #region
+
+      /// <summary>
+      /// Default constructor. Creates a folder with a name..
+      /// </summary>
+      /// <param name="name"></param>
+      public DirectoryFolder( string name )
+      {
+         m_Name = name;
+      }
+   #endregion
+
+   /*PROPERTIES*/
+   #region
+      public List<DirectoryFolder> SubFolders { get { return m_Directories; } }
+      public List<DirectoryFile> Files { get { return m_Files; } }
+   #endregion
+
+   /*OPERATORS*/
+   #region
+   #endregion
+
+   /*STATIC METHODS*/
+   #region
+   #endregion
+
+   /*METHODS*/
+   #region
+
+      /// <summary>
+      /// Gets the size of the folder..
+      /// </summary>
+      /// <returns></returns>
+      public long GetSizeOfFolder( )
+      {
+         long totSize = 0;
+
+      //Add the size of the files in this folder..
+         foreach( DirectoryFile file in m_Files )
+            totSize += file.Size;
+
+      //Loop over all subdirectories..
+         foreach( DirectoryFolder folder in m_Directories )
+            totSize += folder.GetSizeOfFolder( );
+
+      //Return the total size..
+         return totSize;
+      }
+   #endregion
+
+
+   }
+}
